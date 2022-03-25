b3eb045cf20e5036ab1720c419f7eb33d7b63910
Ha <ha030600@gmail.com>
2022-03-25 21:43:56 +0700

    code 116

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./button.scss";
 const Button = (props) => {
   return (
     <button
@@ -12,7 +13,7 @@ const Button = (props) => {
   );
 };
 
-const OutlineButton = (props) => {
+export const OutlineButton = (props) => {
   return (
     <Button
       className={`btn-outline ${props.className}`}
--- src/components/hero-slide/HeroSlide.jsx
+++ src/components/hero-slide/HeroSlide.jsx
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
+
+import SwiperCore, { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import Button, { OutlineButton } from "../button/Button";
+
 import tmdbApi, { category, movieType } from "../../api/tmdbApi";
 import apiConfig from "../../api/apiConfig";
 
+import "./hero-slide.scss";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 const HeroSlide = () => {
+  SwiperCore.use([Autoplay]);
+
   const [movieItems, setMovieItems] = useState([]);
   useEffect(() => {
     const getMovies = async () => {
@@ -20,7 +31,61 @@ const HeroSlide = () => {
     getMovies();
   }, []);
 
-  return <div className="hero-slide ">HeroSlide</div>;
+  return (
+    <div className="hero-slide ">
+      <Swiper
+        modules={[Autoplay]}
+        grabCursor={true}
+        spaceBetween={0}
+        slidesPerView={1}
+      >
+        {movieItems.map((item, i) => (
+          <SwiperSlide key={i}>
+            {({ isActive }) => (
+              <HerosSlideItem
+                item={item}
+                className={`${isActive ? "active" : ""}`}
+              />
+            )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+const HerosSlideItem = (props) => {
+  let history = useHistory();
+
+  const item = props.item;
+
+  const background = apiConfig.originalImage(
+    item.backdrop_path ? item.backdrop_path : item.poster_path
+  );
+
+  return (
+    <div
+      className={`hero-slide__item ${props.className}`}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="hero-slide__item__content container">
+        <div className="hero-slide__item__content__info">
+          <h2 className="title">{item.title}</h2>
+          <div className="overview">{item.overview}</div>
+          <div className="btns">
+            <Button onClick={() => history.push("/movie/" + item.id)}>
+              Watch now
+            </Button>
+            <OutlineButton onClick={() => console.log("trailer")}>
+              Watch trailer
+            </OutlineButton>
+          </div>
+        </div>
+        <div className="hero-slide__item__content__poster">
+          <img src={apiConfig.w500Image(item.poster_path)} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSlide;
