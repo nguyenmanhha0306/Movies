c3a2285d2174eff6c6b2a7b446c424592c86397d
Ha <ha030600@gmail.com>
2022-03-27 18:06:59 +0700

    code 201

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
+import { useParams } from "react-router";
+
 const Catalog = () => {
+  const { category } = useParams();
+
+  console.log(category);
   return <div>Catalog</div>;
 };
 
