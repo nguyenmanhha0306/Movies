95c2ca6b721bf7050d8810c72b9d473c73f49424
Ha <ha030600@gmail.com>
2022-03-27 18:21:11 +0700

    code 207

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,11 +2,21 @@ import React from "react";
 
 import { useParams } from "react-router";
 
+import PageHeader from "../components/page-header/PageHeader";
+
+import { category as cate } from "../api/tmdbApi";
 const Catalog = () => {
   const { category } = useParams();
 
   console.log(category);
-  return <div>Catalog</div>;
+
+  return (
+    <>
+      <PageHeader>
+        {category === cate.movie ? "Movies" : "TV Series"}
+      </PageHeader>
+    </>
+  );
 };
 
 export default Catalog;
